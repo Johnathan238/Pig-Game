1edc275d10803fcde96e712ee4d91eb9b31b8116
Johnathan <jonathanumana86@gmail.com>
2022-01-08 19:44:47 -0500

    Made a function but not working

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+// selecting elements
+const score0ID = document.getElementById('score--0')
+const score1ID = document.getElementById('score--1')
+
+let scoreZero = function (){
+    score0ID.textContent = 0
+    score1ID.textContent = 0
+
+    return scoreZero
+}
+
