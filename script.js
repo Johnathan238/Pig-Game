d7c010674f721dca23f46d02e9c6d7aefa214f84
Johnathan <jonathanumana86@gmail.com>
2022-01-09 17:56:08 -0500

    Made if/else statment for rolled dice

--- script.js
+++ script.js
@@ -3,11 +3,14 @@
 // selecting elements
 const score0ID = document.getElementById('score--0')
 const score1ID = document.getElementById('score--1')
+const current0EL = document.getElementById('current--0')
+const current1EL = document.getElementById('current--1')
 const diceEL = document.querySelector('.dice')
 const btnNew = document.querySelector('.btn--new')
 const btnRoll = document.querySelector('.btn--roll')
 const btnHold = document.querySelector('.btn--hold')
 
+let currentScore = 0
 // Starting Condition
     score0ID.textContent = 0
     score1ID.textContent = 0
@@ -17,13 +20,19 @@ const btnHold = document.querySelector('.btn--hold')
     btnRoll.addEventListener('click', function(){
         // 1. Random Dice roll
         const dice = Math.trunc(Math.random() * 6) + 1 
+        console.log(dice);
 
         // 2. Display dice
         diceEL.classList.remove('hidden')
         diceEL.src = `dice-${dice}.png`
 
         // 3. Check for rolled 1: if tru, switch player 
+        if(dice !== 1){
+            currentScore += dice
+            current0EL.textContent = currentScore
+        } else {
 
+        }
     })
 
 
