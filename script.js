c6ec86bb02c0007c0701e0a00a9d5db7e15f9ae7
Johnathan <jonathanumana86@gmail.com>
2022-01-08 19:50:20 -0500

    Hidden the dice img, Now ik how to hid & will use in Next Project

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 // selecting elements
 const score0ID = document.getElementById('score--0')
 const score1ID = document.getElementById('score--1')
+const diceEL = document.querySelector('.dice')
 
+// Starting Condition
     score0ID.textContent = 0
     score1ID.textContent = 0
+    diceEL.classList.add('hidden')
 
 
 
