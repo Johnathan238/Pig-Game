6071b941f1cbf667f91242811fb4bdaf16246df8
Johnathan <jonathanumana86@gmail.com>
2022-01-09 17:25:04 -0500

    Selecting btn for dice

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const score0ID = document.getElementById('score--0')
 const score1ID = document.getElementById('score--1')
 const diceEL = document.querySelector('.dice')
+const btnNew = document.querySelector('.btn--new')
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold')
 
 // Starting Condition
     score0ID.textContent = 0
