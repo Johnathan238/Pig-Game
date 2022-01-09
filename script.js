7cf80ec58251fd115764608a040fe2461dd267d6
Johnathan <jonathanumana86@gmail.com>
2022-01-09 17:35:07 -0500

    Minpulating the dice SRC  for correct png when landed

--- script.js
+++ script.js
@@ -13,5 +13,18 @@ const btnHold = document.querySelector('.btn--hold')
     score1ID.textContent = 0
     diceEL.classList.add('hidden')
 
+    // Rolling dice function
+    btnRoll.addEventListener('click', function(){
+        // 1. Random Dice roll
+        const dice = Math.trunc(Math.random() * 6) + 1 
+
+        // 2. Display dice
+        diceEL.classList.remove('hidden')
+        diceEL.src = `dice-${dice}.png`
+
+        // 3. Check for rolled 1: if tru, switch player 
+
+    })
+
 
 
