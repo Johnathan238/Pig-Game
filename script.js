9ac0e4a93b8886459c27971d6399565fdc1e09c4
Johnathan <jonathanumana86@gmail.com>
2022-01-08 19:45:48 -0500

    I removed the function for the Score-ID

--- script.js
+++ script.js
@@ -4,10 +4,8 @@
 const score0ID = document.getElementById('score--0')
 const score1ID = document.getElementById('score--1')
 
-let scoreZero = function (){
     score0ID.textContent = 0
     score1ID.textContent = 0
 
-    return scoreZero
-}
+
 
