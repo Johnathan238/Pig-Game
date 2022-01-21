d820aaca20dc9e13db01082f73faf5a1e0816601
Johnathan238 <jonathanumana86@gmail.com>
2022-01-21 01:56:33 -0500

    the score on the DOM wasn't changing but i was calling the wrong Ele

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ btnHold.addEventListener('click', function () {
   // 1. Add current score
   scores[activePlayer] += currentScore;
 
-  document.getElementById(`current--${activePlayer}`).textContent =
+  document.getElementById(`score--${activePlayer}`).textContent =
     scores[activePlayer];
   // 2. Check if player's score is >= 100
   // Finish the game
