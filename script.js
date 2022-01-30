d47ae844056601efa1416c7f9044be5b3b699315
Johnathan238 <jonathanumana86@gmail.com>
2022-01-29 21:06:39 -0500

    Added Playing boolen so when player wins, they won't be able to click buttons anymore

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const btnHold = document.querySelector('.btn--hold');
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -32,38 +33,44 @@ diceEL.classList.add('hidden');
 
 // Rolling dice function
 btnRoll.addEventListener('click', function () {
-  // 1. Random Dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
+  if (playing) {
+    // 1. Random Dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
-  // 2. Display dice
-  diceEL.classList.remove('hidden');
-  diceEL.src = `dice-${dice}.png`;
+    // 2. Display dice
+    diceEL.classList.remove('hidden');
+    diceEL.src = `dice-${dice}.png`;
 
-  // 3. Check for rolled 1: if tru, switch player
-  if (dice !== 1) {
-    currentScore += dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-    // current0EL.textContent = currentScore
-  } else {
-    switchPlayer()
-  }
-});
+    // 3. Check for rolled 1: if tru, switch player
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+      // current0EL.textContent = currentScore
+    } else {
+      switchPlayer()
+    }
+  }});
 
 btnHold.addEventListener('click', function () {
-  // 1. Add current score
-  scores[activePlayer] += currentScore;
+  if (playing) {
+    // 1. Add current score
+    scores[activePlayer] += currentScore;
 
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
-  // 2. Check if player's score is >= 100
-  if (scores[activePlayer] >= 100) {
-    document.querySelector(`player--${activePlayer}`).classList.add('player--winner')
-    document.querySelector(`player--${activePlayer}`).classList.remove('player--active')
-  }
-  // Finish the game
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      playing = false
+      diceEL.classList.add('hidden');
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner')
+      document.querySelector(`.player--${activePlayer}`).classList.remove('player--active')
+    } else {
 
-  // Switch to the next player
-  switchPlayer()
-});
+      // Finish the game
+  
+      // Switch to the next player
+      switchPlayer()
+    }
+  }});
